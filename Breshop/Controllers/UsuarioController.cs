611a9849bc36f66f002afbe58e268844cdca41cd
lab54 <lab54@FATECRL.COM>
2022-10-17 21:20:05 -0300

    Desenvolvido tela de login

---
+++ Breshop/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Breshop.Models;
+
+namespace Breshop.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        public UsuarioController()
+        {
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            try
+            {
+                Usuario usuario = new Usuario();
+
+                return View(usuario);
+            }
+            catch (Exception)
+            {
+                Usuario usuario = new Usuario();
+
+                return View(usuario);
+            }
+        }
+
+        public async Task<IActionResult> LogarUsuario()
+        {
+            try
+            {
+                Usuario usuario = new Usuario();
+
+                return View(usuario);
+            }
+            catch (Exception)
+            {
+                Usuario usuario = new Usuario();
+
+                return View(usuario);
+            }
+        }
+    }
+}
